ba910b2158142216cd1ab955ce2f3526aebcdfcd
shriyashthakur <109011168+shriyashthakur@users.noreply.github.com>
2022-11-04 23:45:33 +0530

    Print_Rhombus

---
+++ Print_Rhombus.cpp
@@ -0,0 +1,22 @@
+//Write a Program to print Hollow Rhombus pattern of printing stars. 
+#include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cin>> n;
+  for (int i=0; i < n; i++)
+  {
+    for (int j=0; j < n - i; j++)
+      cout << " ";
+    for (int j=0; j < n; j++)
+    {
+      if ((i == 0) || (i == n-1) || (j == 0) || (j == n-1))
+        cout << "*" ;
+      else 
+        cout << " " ;
+    }
+    cout << endl ;
+  }
+  return 0;
+}
